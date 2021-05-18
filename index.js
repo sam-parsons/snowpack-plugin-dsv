bad4e673a543f704b6361da5f41d1fe66d75eb52
Sam Parsons <samparsons269@gmail.com>
2021-05-18 10:40:59 -0400

    added tsv support to plugin

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { csvParse } = require('d3-dsv');
+const { csvParse, tsvParse } = require('d3-dsv');
 const toSource = require('tosource');
 const fs = require('fs');
 const path = require('path');
@@ -7,19 +7,23 @@ module.exports = function plugin(config, options) {
   console.log('plugin invoked');
   // console.log('config', config);
   // console.log('options', options);
+  const parsers = {
+    '.csv': csvParse,
+    '.tsv': tsvParse,
+  };
   return {
     name: 'snowpack-plugin-dsv',
     resolve: {
-      input: ['.csv'],
+      input: ['.csv', '.tsv'],
       output: ['.js'],
     },
     load({ filePath, fileExt }) {
       console.log('load event');
       console.log('filePath', filePath);
       console.log('fileExt', fileExt);
-      const csv = fs.readFileSync(filePath);
-      console.log('csv', csv.toString());
-      const rows = csvParse(csv.toString());
+      const file = fs.readFileSync(filePath);
+      console.log('csv', file.toString());
+      const rows = parsers[fileExt](file.toString());
       console.log('rows', rows);
       return `export default ${toSource(rows)};`;
     },
