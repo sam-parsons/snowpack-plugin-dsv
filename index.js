50d26a2b7830684ab83b146fcee27eaa6c281699
Sam Parsons <samparsons269@gmail.com>
2021-05-18 01:06:46 -0400

    plugin parses csv, return js

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+const { csvParse } = require('d3-dsv');
+const toSource = require('tosource');
+const fs = require('fs');
+const path = require('path');
+
 module.exports = function plugin(config, options) {
   console.log('plugin invoked');
-  console.log('config', config);
-  console.log('options', options);
-  return true;
+  // console.log('config', config);
+  // console.log('options', options);
+  return {
+    name: 'snowpack-plugin-dsv',
+    resolve: {
+      input: ['.csv'],
+      output: ['.js'],
+    },
+    load({ filePath, fileExt }) {
+      console.log('load event');
+      console.log('filePath', filePath);
+      console.log('fileExt', fileExt);
+      const csv = fs.readFileSync(filePath);
+      console.log('csv', csv.toString());
+      const rows = csvParse(csv.toString());
+      console.log('rows', rows);
+      return `export default ${toSource(rows)};`;
+    },
+  };
 };
