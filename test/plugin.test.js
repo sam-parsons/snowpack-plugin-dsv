064aa9e734dcbad8ae3ecc539e7c37cebb59750d
Sam Parsons <samparsons269@gmail.com>
2021-05-19 16:26:52 -0400

    added tests for csv and tsv transformation via snapshot

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -4,4 +4,22 @@ describe('snowpack dsv plugin', () => {
   test('invokes plugin without error', () => {
     expect(plugin()).toHaveProperty('name');
   });
+
+  test('load function return value matches snapshot with csv input', () => {
+    const obj = plugin();
+    const res = obj.load({
+      fileExt: '.csv',
+      filePath: './test/fixtures/data.csv',
+    });
+    expect(res).toMatchSnapshot();
+  });
+
+  test('load function return value matches snapshot with tsv input', () => {
+    const obj = plugin();
+    const res = obj.load({
+      fileExt: '.tsv',
+      filePath: './test/fixtures/data.tsv',
+    });
+    expect(res).toMatchSnapshot();
+  });
 });
