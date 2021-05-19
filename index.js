92c9bea700d7f2930520a6b80a490b022e26be5f
Sam Parsons <samparsons269@gmail.com>
2021-05-19 16:24:45 -0400

    cleaned up plugin, added dynamic parsing

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const { csvParse, tsvParse } = require('d3-dsv');
 const toSource = require('tosource');
 const fs = require('fs');
-const path = require('path');
 
 module.exports = function plugin(config, options) {
-  console.log('plugin invoked');
-  // console.log('config', config);
-  // console.log('options', options);
   const parsers = {
     '.csv': csvParse,
     '.tsv': tsvParse,
   };
+
   return {
     name: 'snowpack-plugin-dsv',
     resolve: {
@@ -18,13 +15,8 @@ module.exports = function plugin(config, options) {
       output: ['.js'],
     },
     load({ filePath, fileExt }) {
-      console.log('load event');
-      console.log('filePath', filePath);
-      console.log('fileExt', fileExt);
       const file = fs.readFileSync(filePath);
-      console.log('csv', file.toString());
       const rows = parsers[fileExt](file.toString());
-      console.log('rows', rows);
       return `export default ${toSource(rows)};`;
     },
   };
