181c791d8afc2bae3668db119af7fd0a9412a406
Sam Parsons <samparsons269@gmail.com>
2021-05-20 21:36:39 -0400

    added psv and dsv support with dsvFormat method

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
-const { csvParse, tsvParse } = require('d3-dsv');
+const { csvParse, tsvParse, dsvFormat } = require('d3-dsv');
 const toSource = require('tosource');
 const fs = require('fs');
 
 module.exports = function plugin(config, options) {
+  options = Object.assign({}, options);
+
   const parsers = {
     '.csv': csvParse,
     '.tsv': tsvParse,
+    '.dsv': dsvFormat,
   };
 
   return {
     name: 'snowpack-plugin-dsv',
     resolve: {
-      input: ['.csv', '.tsv'],
+      input: ['.psv', '.csv', '.tsv', '.dsv'],
       output: ['.js'],
     },
     load({ filePath, fileExt }) {
       const file = fs.readFileSync(filePath);
-      const rows = parsers[fileExt](file.toString());
+      let rows;
+      if (options.delimiter) {
+        const parser = parsers['.dsv'](options.delimiter);
+        rows = parser.parseRows(file.toString());
+      } else {
+        rows = parsers[fileExt](file.toString());
+      }
       return `export default ${toSource(rows)};`;
     },
   };
