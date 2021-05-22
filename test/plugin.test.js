39bb41cb510c2a3c211df969eb82def61d2ae4a0
Sam Parsons <samparsons269@gmail.com>
2021-05-21 21:15:44 -0400

    refactor base test, add multiple delimiter case

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -1,8 +1,14 @@
 const plugin = require('../index.js');
 
 describe('snowpack dsv plugin', () => {
-  test('invokes plugin without error', () => {
-    expect(plugin()).toHaveProperty('name');
+  test('invokes plugin and returns correctly shaped object without error', () => {
+    const obj = plugin();
+    expect(obj.name).toEqual('snowpack-plugin-dsv');
+    expect(obj.resolve).toEqual({
+      input: ['.psv', '.csv', '.tsv', '.dsv'],
+      output: ['.js'],
+    });
+    expect(typeof obj.load).toEqual('function');
   });
 
   test('load function return value matches snapshot with csv input', () => {
@@ -41,12 +47,37 @@ describe('snowpack dsv plugin', () => {
     expect(res).toMatchSnapshot();
   });
 
+  test('load function returns error when parsing .dsv file without delimiter options object', () => {
+    const obj = plugin();
+    const error = obj.load({
+      fileExt: '.dsv',
+      filePath: './test/fixtures/data.dsv',
+    });
+    expect(error.message).toEqual(
+      'No delimited provided for use with .dsv file'
+    );
+  });
+
   test('load function return value matches snapshot with custom file extension', () => {
     const obj = plugin({}, { delimiters: ['.~sv'] });
     const res = obj.load({
-      fileExt: '.dsv',
+      fileExt: '.~sv',
       filePath: './test/fixtures/data.~sv',
     });
     expect(res).toMatchSnapshot();
+  });
+
+  test('load function return value matches snapshot with multiple custom file extensions', () => {
+    const obj = plugin({}, { delimiters: ['.~sv', '.%sv'] });
+    // both files should result in the same string
+    const resI = obj.load({
+      fileExt: '.~sv',
+      filePath: './test/fixtures/data.~sv',
+    });
+    const resII = obj.load({
+      fileExt: '.%sv',
+      filePath: './test/fixtures/data.%sv',
+    });
+    expect(resI).toEqual(resII);
   });
 });
